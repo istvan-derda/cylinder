7edfe51df4b39552c25d2e3c3e68f780af7c05ba
Istvan Derda <istvan.derda@stud.htwk-leipzig.de>
2020-05-02 18:23:18 +0200

    review Bine

--- main.cpp
+++ main.cpp
@@ -2,6 +2,10 @@
 #include <fstream>
 #include "Cylinder.cpp"
 
+void printMessage(std::string message) {
+    std::cout << message << std::endl;
+}
+
 int main() {
     double height;
     double radius;
@@ -12,29 +16,28 @@ int main() {
     std::cin >> height;
     std::cout << "Enter the RADIUS of the cylinder" << std::endl;
     std::cin >> radius;
-
     do {
         std::cout << "Enter the desired NUMBER OF SIDES of the cylinder when rendered (min 3)" << std::endl;
         std::cin >> resolution;
     } while (resolution < 3);
 
-    // an dieser Stelle switch-case?
-    // Satzanfang groß
-    // std::cout vllt auslagern und die verschiedenen ausgabenin eine variable packen und nach dem switch ausgeben
+
     if (resolution < 7) {
-        std::cout << "edgy!" << std::endl;
+        std::cout << "Edgy!" << std::endl;
     } else if (resolution < 20) {
-        std::cout << "here you go!" << std::endl;
+        std::cout << "Here you go!" << std::endl;
     } else if (resolution < 100) {
-        std::cout << "mmh, so smooth" << std::endl;
+        std::cout << "Mmh, so smooth" << std::endl;
     } else if (resolution < 5000) {
-        std::cout << "you must have an amazing printer!" << std::endl;
+        std::cout << "You must have an amazing printer!" << std::endl;
     } else if (resolution < 50000) {
-        std::cout << "yea, I can still do that" << std::endl;
+        std::cout << "Yea, I can still do that" << std::endl;
     } else if (resolution < 500000) {
-        std::cout << "ok, give me a sec" << std::endl;
-    } else if (resolution >= 500000) {
-        std::cout << "are you trying to heat your room with a cpu?" << std::endl;
+        std::cout << "Ok, give me a sec" << std::endl;
+    } else if (resolution < 1000000) {
+        printMessage("Are you trying to heat your room with a CPU?");
+    } else if (resolution >= 1000000) {
+        printMessage("This is a benchmark, right?");
     }
 
     Cylinder cylinder = Cylinder(height, radius);
